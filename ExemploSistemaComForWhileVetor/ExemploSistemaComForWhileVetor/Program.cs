d77698a371a34119d3449d3790b2455c5334ea5a
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-05-03 16:38:04 -0300

    Edição de exemplo

--- ExemploSistemaComForWhileVetor/ExemploSistemaComForWhileVetor/Program.cs
+++ ExemploSistemaComForWhileVetor/ExemploSistemaComForWhileVetor/Program.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NossaAplicacao
+namespace ExemploSistemaComForWhileVetor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Eu \"Bárbara\" quero imprimir");
+            new Sistema();
+
         }
     }
 }
---
+++ ExemploSistemaComForWhileVetor/ExemploSistemaComForWhileVetor/Sistema.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExemploSistemaComForWhileVetor
+{
+    class Sistema
+    {
+        Barco barco = new Barco();
+        public Sistema()
+        {
+            GerirMenu();
+        }
+        // Apresentar o menu para o usuário
+        public void Menu()
+        {
+            Console.WriteLine(@"1 - Cadastro de barcos 
+2 - Editar barco 
+3 - Listar barcos
+4 - Buscar barco
+20 - Sair");
+            
+        }
+
+        public void GerirMenu()
+        {
+            // Chama o método para apresentar o menu para o usuário
+            Menu();
+            int menuEscolha = Convert.ToInt32(Console.ReadLine());
+            while (menuEscolha != 20)
+            {
+
+                //Como se fosse um "if", porém com "==" 
+
+                switch (menuEscolha)
+                {
+                    case 1:
+                        barco.CadastrarBarco();
+                        break;
+                    case 2:
+                        barco.EditarBarco();
+                        break;
+                    case 3:
+                        barco.ListarBarcos();
+                        break;
+                    case 4:
+                        barco.BuscarBarco();
+                        break;
+
+                        
+                }
+                Menu();
+                menuEscolha = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+
+
+}
